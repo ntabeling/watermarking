cda82764bf1bd6eec4b354b22d0dc95eb1e228cd
Nate <ntabeling@gmail.com>
2019-04-13 18:59:36 -0400

    got multiple watermarks working

--- createWatermark.py
+++ createWatermark.py
@@ -1,18 +1,14 @@
 import configparser
+from shutil import copyfile
 from PIL import Image, ImageDraw, ImageFont
 from wmDataPrep import wmDictCreator
-
-config = configparser.ConfigParser()
-config.read('./config.ini')
-
-files = wmDictCreator(config['PATHS']['incsv'])
 
 
 def createWatermark(filename, watermark, xmod, ymod):
     text = watermark
     color = 'white'
     fontfamily = 'arial.ttf'
-    image = Image.open(config['PATHS']['imgIn'] + filename).convert('RGBA')
+    image = Image.open(config['PATHS']['imgout'] + filename).convert('RGBA')
     imageWatermark = Image.new('RGBA', image.size, (255, 255, 255, 0))
     draw = ImageDraw.Draw(imageWatermark)
     width, height = image.size
@@ -21,10 +17,17 @@ def createWatermark(filename, watermark, xmod, ymod):
     x = width * xmod
     y = height * ymod
     draw.text((x, y), text, color, font)
+    wmImg = Image.alpha_composite(image, imageWatermark)
+    wmImg.convert('RGB').save(config['PATHS']['imgOut'] + filename)
+    return f"{filename}\t{watermark}"
 
 
-def applyWatermark(filename, wm1, wm2):
-    image = Image.open(config['PATHS']['imgIn'] + filename).convert('RGBA')
-    wmImg = Image.alpha_composite(image, wm1, wm2)
-    wmImg.convert('RGB').save(config['PATHS']['imgOut'] + 'water_' + filename)
-    return config['PATHS']['imgOut'] + 'water_' + filename
+config = configparser.ConfigParser()
+config.read('./configs/config.ini')
+
+files = wmDictCreator(config['PATHS']['incsv'])
+
+for k, v in files.items():
+    copyfile(config['PATHS']['imgin'] + k, config['PATHS']['imgout'] + k)
+    for key, val in files[k].items():
+        createWatermark(k, val[0], val[1], val[2])
